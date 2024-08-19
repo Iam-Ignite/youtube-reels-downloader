551d541c0999b93ef6621cff18007ea327986b65
uru <ur>
2024-08-20 00:59:41 +0100

    jf

--- src/app/components/HowItWorks.js
+++ src/app/components/HowItWorks.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const HowItWorks = () => {
@@ -9,7 +10,7 @@ const HowItWorks = () => {
           <span className="text-[#d03547]">YouTube</span> Downloader
         </h2>
         <div className="md:flex justify-center gap-16">
-          <img src="/icon/person.png" alt="Person downloading video" className="md:h-96 h-60 m-auto mb-4" />
+          <Image src="/icon/person.png" alt="Person downloading video" className="md:h-96 h-60 m-auto mb-4" />
           <div className="grid grid-cols-1 gap-8">
             <div className="flex items-center gap-6">
               <h3 className="text-lg border-2 border-[#d03547] text-[#d03547] md:h-20 flex justify-center items-center w-14 h-14 md:w-20 rounded-full md:text-2xl font-semibold mb-2">01</h3>
